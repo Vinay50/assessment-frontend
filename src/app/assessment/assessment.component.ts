ef10d60cd5a05c6dc1fd1020c97a1ca5a1033b06
Vieenay Siingh <vieenaysiingh@gmail.com>
2018-10-08 00:37:21 +0530

    traversing questions with answers added

--- src/app/assessment/assessment.component.ts
+++ src/app/assessment/assessment.component.ts
@@ -11,16 +11,10 @@ export class AssessmentComponent implements OnInit {
   assessments: any[];
   mode = 'quiz';
   assessmentName: string;
-  ques: any;
-  answer: any;
-  pager = {
-    index: 0,
-    size: 1,
-    count: 1
-  };
-
+  userIndex: any;
+  selectedOption: any;
   assessmentId: any;
-  assessmentDetails: any;
+  assessmentDetail: any;
   showQuestion: Boolean = false;
   questionDetails: any;
 
@@ -30,49 +24,28 @@ export class AssessmentComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.userIndex = 0;
     this.assessmentId = this.activatedRoute.snapshot.params.id;
     this.assessmentService.getAssessment(this.assessmentId).subscribe(res => {
-      this.assessmentDetails = res;
+      this.assessmentDetail = res;
     });
   }
   startAsessment() {
     this.showQuestion = true;
     this.assessmentService.getQuestions(this.assessmentId).subscribe(res => {
       this.questionDetails = res;
-      console.log('Qestion', this.questionDetails);
-      this.questionDetails.map(q => {
-        this.ques = q;
-        console.log('qqqqqqqqqq', this.ques);
-      });
-      this.ques.answers.map(ans => {
-        this.answer = ans;
-        console.log('aaaaaaa', this.answer);
-      });
-      // this.onSelect(this.ques, this.answer);
     });
   }
 
-  get filteredQuestions() {
-    return this.assessment.questions
-      ? this.assessment.questions.slice(
-          this.pager.index,
-          this.pager.index + this.pager.size
-        )
-      : [];
-  }
-
-  onSelect(question: any, answer: any) {
-    console.log('inside', this.questionDetails);
-    question.answers.forEach(x => {
-      if (x.id !== option.id) x.selected = false;
-    });
-    this.goTo(this.pager.index + 1);
-  }
-
-  goTo(index: number) {
-    if (index >= 0 && index < this.pager.count) {
-      this.pager.index = index;
-      this.mode = 'quiz';
+  changeIndex(number, answerid) {
+    this.selectedOption = this.questionDetails[this.userIndex].answers.filter(
+      item => item.id === answerid
+    )[0];
+    if (
+      (this.userIndex > 0 && number < 0) ||
+      (this.userIndex < this.questionDetails.length && number > 0)
+    ) {
+      this.userIndex += number;
     }
   }
 }
